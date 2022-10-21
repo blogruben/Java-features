61a23b3570c021eb53ad57adaf6a7f8ad2ea8d15
99GU8572 <ruben.ruiz2@at.seg-social.es>
2022-10-21 12:22:23 +0200

    init ejemplos de condicionales y variables no iniciadas

---
+++ Java-ejemplos-cortos/src/variable/noIniciados/MainlocalVariablesNotInitialized.java
@@ -0,0 +1,24 @@
+package variable.noIniciados;
+
+
+public class MainlocalVariablesNotInitialized {
+
+	public static void main(String[] args) {
+//		If the programmer, by mistake, did not initialize a local variable 
+//		and it takes a default value, then the output could be some unexpected value. 
+//		So in case of local variables, the compiler will ask the programmer to initialize 
+//		it with some value before they access the variable to avoid the usage of undefined values.
+	
+		
+//		Persona p;
+//		
+//		if( p != null) {
+//			  System.out.println("Bingo");
+//		}
+		
+	}
+	
+	
+
+
+}
